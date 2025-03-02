3af97a7b62cadd16aeda7bafa2aa18c591300138
lundg <id23oln@cs.umu.se>
2025-03-02 12:45:59 +0100

    fixat en logga in funktion

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-document.getElementById('logga-in').onclick = function () {
-        document.getElementById('logga-inRuta').style.display = 'block';
+let loggaInCounter = 0;
+
+const loggaInRuta = function () {
+        let loggaIn = document.getElementById("logga-inRuta");
+        if(loggaInCounter %2 === 0) {
+                loggaIn.style.display = 'block';
+                setTimeout(() => {
+                        loggaIn.classList.add('show');
+                }, 10);
+        }
+        else {
+                loggaIn.classList.toggle('show');
+
+                setTimeout(() => {
+                        loggaIn.style.display = 'none';
+                }, 300);
+
+        }
+        loggaInCounter++;
 }
+document.getElementById('logga-in').addEventListener('click',loggaInRuta);
+
