f8420681385d86d0a4349df389f5ac5f6b098aa0
lundg <id23oln@cs.umu.se>
2025-03-07 16:57:43 +0100

    småfix

--- js/script.js
+++ js/script.js
@@ -23,5 +23,76 @@ let LyckseleNPin = L.marker([64.59796667960984, 18.703203778002226], {icon:gymIc
 let luleaBPin = L.marker([65.61474405959379, 22.17745233961311], {icon:gymIcon}).addTo(karta);
 
 
+const gymData = [
+    {
+        id: "Anderstorp",
+        name: "Anderstorp",
+        coordinates: [64.73580529107286, 20.971807439606952]
+    },
+    {
+        id: "Centrum",
+        name: "skeåCentrum",
+        coordinates: [64.75041430382274, 20.95028279408538]
+    },
+    {
+        id: "Bodenarena",
+        name: "Boden A",
+        coordinates: [65.81179835457746, 21.697834053802797]
+    },
+    {
+        id: "Bodenwinternet",
+        name: "Boden W",
+        coordinates: [65.82475244446813, 21.66242401474603]
+    },
+    {
+        id: "Furunäset",
+        name: "Furunäset",
+        coordinates: [65.29392361192079, 21.488289881912436]
+    },
+    {
+        id: "Backcity",
+        name: "Backcity",
+        coordinates: [65.32353504571843, 21.443532408900833]
+    },
+    {
+        id: "LyckseleC",
+        name: "Lycksele C",
+        coordinates: [64.59444413895397, 18.678419197191804]
+    },
+    {
+        id: "Gammelstad",
+        name: "Luleå G",
+        coordinates: [65.6416891857714, 22.01362478686077]
+    },
+    {
+        id: "Porsön",
+        name: "Luleå P",
+        coordinates: [65.61913511348868, 22.148037864749156]
+    },
+    {
+        id: "LyckseleN",
+        name: "Lycksele N",
+        coordinates: [64.59796667960984, 18.703203778002226]
+    },
+    {
+        id: "Björksgatan",
+        name: "Luleå B",
+        coordinates: [65.61474405959379, 22.17745233961311]
+    }
+]
+
+let showOnMap = function (gymID) {
+    let gym = gymData.find(g=>g.id===gymID);
+    if(gym){
+        karta.setView(gym.coordinates,15);
+    }
+}
+document.querySelectorAll('.gym').forEach(gym => {
+
+    gym.addEventListener('click', function (){
+        showOnMap(gym.id)
+    })
+})
+
 
 
